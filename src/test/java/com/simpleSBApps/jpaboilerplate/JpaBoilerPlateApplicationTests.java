0c2dee5c1d146446122986c678f5e867f5fbc08f
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2020-11-21 22:19:36 -0500

    initialized project

---
+++ src/test/java/com/simpleSBApps/jpaboilerplate/JpaBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleSBApps.jpaboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
